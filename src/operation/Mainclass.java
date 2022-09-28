ce54f3ad7c97ecf1426ae84db3c5f9edc22fc0cf
1537639 <1992425366@qq.com>
2022-09-28 17:07:18 +0800

    Changes

--- src/operation/Mainclass.java
+++ src/operation/Mainclass.java
@@ -17,16 +17,16 @@ public class Mainclass {
         //输入界定生成数范围
 
         //进行n次以下操作
-        for(int i=0;i<exercise_num;i++)
+        /*for(int i=0;i<exercise_num;i++)
         {
             exercise=g_e.generate(limit_num);//生成一个题目
             answer=a_r.answer(exercise);//生成答案
             if(answer==null)
                 continue;//如果返回答案是空（为负数），则跳过这次循环
             o_r.output(exercise,answer);//输出到题目与答案文件
-        }
+        }*/
 
         //判断答案是否正确，输出分数到grade.txt
-        c_r.check();
+        //c_r.check();
     }
 }
---
+++ src/operation/number.java
@@ -0,0 +1,12 @@
+package operation;
+
+public class number {
+    int numer;
+    int denomin;
+    public number(int numer_,int denomin_)//分子，分母
+    {
+        this.numer=numer_;
+        this.denomin=denomin_;
+    }
+
+}
